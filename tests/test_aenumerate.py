e96bd1f72c9b70ef65daed07170393a3d2716285
João S. O. Bueno <gwidion@gmail.com>
2024-10-24 00:33:31 -0300

    extra test

--- tests/test_aenumerate.py
+++ tests/test_aenumerate.py
@@ -19,3 +19,15 @@ async def test_aenumerate_works():
     async for i, step in aenumerate(counter(4)):
         assert i == step
 
+
+@pytest.mark.asyncio
+async def test_aenumerate_start_arg():
+
+    async def counter(n):
+        for i in range(n):
+            yield i
+            await asyncio.sleep(DELAY)
+
+    async for i, step in aenumerate(counter(4), 1):
+        assert i - 1 == step
+
