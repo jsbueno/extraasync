a492b3c5e226ab964b1de60d833aa9cb42c645d4
João S. O. Bueno <gwidion@gmail.com>
2024-11-15 22:52:26 -0300

    Make aenumerate.index readonly]

--- extraasync/aenumerate.py
+++ extraasync/aenumerate.py
@@ -2,17 +2,21 @@ class aenumerate:
     def __init__(self, iterable, start=None):
         self.iterable = iterable
         self.start = start or 0
-        self.index = self.start
+        self._index = self.start
+
+    @property
+    def index(self):
+        return self._index
 
     async def __aiter__(self):
-        self.index
         while True:
             try:
                 result = await anext(self.iterable)
                 yield self.index, result
             except StopAsyncIteration:
                 break
-            self.index += 1
+            self._index += 1
 
     def __repr__(self):
-        return f"Asynchronous enumeration for {self.iterable} at index {self.index}"
+        return f"Asynchronous enumeration for {self.iterable!r} at index {self.index!r}"
+
