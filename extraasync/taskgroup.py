a9fb701d917d619c6d1d73b963c07f08997a1bed
Joao S O Bueno <gwidion@gmail.com>
2025-02-13 12:35:08 -0300

    Enable classic 'cancel all tasks' behavior for ExtraTaskGroup

--- extraasync/taskgroup.py
+++ extraasync/taskgroup.py
@@ -17,4 +17,33 @@ if not hasattr(TaskGroup, "_abort"):
 
 
 class ExtraTaskGroup(TaskGroup):
-    _abort = lambda self: None
+    def __init__(self, *, default_abort: bool = False):
+        """A subclass of asyncio.TaskGroup
+
+        By default, the different behavior is that if a
+        taks in the group fails and raises any exception, the other
+        tasks _are_ _not_ cancelled, and are allowed to run
+        to completion.
+
+        The exceptions raised in all tasks are grouped in
+        an ExcpetionGroup (which can be caught with PEP 654's
+        `except *Exeption` statement - just like asyncio.TaskGroups,
+        the difference being is that by not cancelling all other
+        tasks, the group may, in fact, contain more than a
+        single exception.
+
+        Args:
+            default_abort: if True, allows the default asyncio.TaskGroup behavior
+            or aborting all other running tasks when the first one raises an exception.
+
+
+        """
+        self.default_abort = default_abort
+        super().__init__()
+
+    def _abort(self):
+        if self.default_abort:
+            return super()._abort()
+        return None
+
+
--- tests/test_taskgroup.py
+++ tests/test_taskgroup.py
@@ -10,21 +10,46 @@ from extraasync import ExtraTaskGroup
 
 
 @pytest.mark.asyncio
-async def test_extrataskgroup_works():
+async def test_extrataskgroup_default_not_abort_works():
     async def worker(n):
         await asyncio.sleep(n/30)
         if n % 3 == 0:
             raise RuntimeError()
         return n
 
+    exception_group_handled = False
     try:
         async with ExtraTaskGroup() as tg:
             tasks = [tg.create_task(worker(i)) for i in range(10)]
     except *RuntimeError as exceptions:
-        assert len(exceptions.exceptions) == 4
+        exception_group_handled = True
+        assert len(exceptions.exceptions) == 4  # indexes 0, 3, 6 and 9.
 
+    assert sum(int(task.cancelled()) for task in tasks) == 0
+    assert exception_group_handled, "Exception 'RuntimeError' not raised in group"
     assert not any(task.exception() == asyncio.exceptions.CancelledError for task in tasks)
     assert {task.result() for task in tasks if not task.exception()} == {i for i in range(10) if i % 3}
+
+
+@pytest.mark.asyncio
+async def test_extrataskgroup_opt_in_abort():
+    async def worker(n):
+        await asyncio.sleep(n/30)
+        if n % 3 == 0:
+            raise RuntimeError()
+        return n
+
+    exception_group_handled = False
+    try:
+        async with ExtraTaskGroup(default_abort=True) as tg:
+            tasks = [tg.create_task(worker(i)) for i in range(10)]
+    except *RuntimeError as exceptions:
+        exception_group_handled = True
+        assert len(exceptions.exceptions) == 1
+    assert sum(int(task.cancelled()) for task in tasks) == 9
+
+    assert exception_group_handled, "Exception 'RuntimeError' not raised in group"
+    assert {task.result() for task in tasks if not task.cancelled() and not task.exception()} == set()
 
 
 def test_package_warns_if_taskgroup_implementation_becomes_incompatible():
@@ -38,5 +63,6 @@ def test_package_warns_if_taskgroup_implementation_becomes_incompatible():
         with pytest.warns():
             from extraasync.taskgroup import ExtraTaskGroup
     import asyncio.taskgroups
+
 
 
