81dc7f4cf0ce7d271fb992d6db683da3da4f3c26
Joao Bueno <gwidion@gmail.com>
2025-05-22 03:14:57 -0300

    [WIP] Pipeline: Rate limiter class

--- extraasync/__init__.py
+++ extraasync/__init__.py
@@ -2,9 +2,9 @@ from .aenumerate import aenumerate
 from .taskgroup import ExtraTaskGroup
 from .sync_async_bridge import sync_to_async, async_to_sync
 from .async_hooks import at_loop_stop_callback, remove_loop_stop_callback
-from .pipeline import Pipeline
+from .pipeline import Pipeline, RateLimiter
 
 __version__ = "0.3.0"
 
 
-__all__ = ["aenumerate", "ExtraTaskGroup", "sync_to_async", "async_to_sync", "at_loop_stop_callback", "pipeline", "remove_loop_stop_callback",  "__version__"]
+__all__ = ["aenumerate", "ExtraTaskGroup", "sync_to_async", "async_to_sync", "at_loop_stop_callback", "Pipeline", "remove_loop_stop_callback", "RateLimiter", "__version__"]
